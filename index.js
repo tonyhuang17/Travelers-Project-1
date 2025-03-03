d9fa3feca75e5d051ae2fa79a6e2a4eeee0432fd
Bob Ross <puffyCloud79@bobross.net>
2025-03-03 15:28:48 -0500

    Added and configured based planet HTML, JS files - Running webpage with Planets ONLY

--- index.js
+++ index.js
@@ -1,39 +1,81 @@
-let characters = [];
-let matchingCharacters = [];
-const charactersList = document.querySelector("#charactersList")
+// let characters = [];
+// let matchingCharacters = [];
+// const charactersList = document.querySelector("#charactersList")
 
-document.addEventListener('DOMContentLoaded', getCharacters)
+// document.addEventListener('DOMContentLoaded', getCharacters)
 
-async function getCharacters() {
-  let url = 'http://localhost:9001/api/characters';
+// async function getCharacters() {
+//   let url = 'http://localhost:9001/api/characters';
+
+//   try {
+//     const fetchedCharacters = await fetch(url)
+//       .then(res => res.json())
+//     characters.push(...fetchedCharacters);
+//   }
+//   catch (ex) {
+//     console.error("Error reading characters.", ex.message);
+//   }
+//   console.log("All the characters are ", characters)
+//   renderCharacters(characters);
+// }
+
+// const filterCharacters = () => {
+//   const searchString = document.querySelector("#searchString").value;
+//   const re = new RegExp(searchString, "i");
+//   matchingCharacters = characters.filter(character => re.test(character.name))
+//   renderCharacters(matchingCharacters);
+// }
+
+// const renderCharacters = characters => {
+//   const divs = characters.map(character => {
+//     const el = document.createElement('div');
+//     el.addEventListener('click', () => goToCharacterPage(character.id));
+//     el.textContent = character.name;
+//     return el;
+//   })
+//   charactersList.replaceChildren(...divs)
+// }
+
+// const goToCharacterPage = id => window.location = `/character.html?id=${id}`
+
+
+// Planet Search
+let planets = [];
+let matchingPlanets = [];
+const planetsList = document.querySelector("#planetsList")
+
+document.addEventListener('DOMContentLoaded', getPlanets)
+
+async function getPlanets() {
+  let url = 'http://localhost:9001/api/planets';
 
   try {
-    const fetchedCharacters = await fetch(url)
+    const fetchedPlanets = await fetch(url)
       .then(res => res.json())
-    characters.push(...fetchedCharacters);
+    planets.push(...fetchedPlanets);
   }
   catch (ex) {
-    console.error("Error reading characters.", ex.message);
+    console.error("Error reading planets.", ex.message);
   }
-  console.log("All the characters are ", characters)
-  renderCharacters(characters);
+  console.log("All the planets are ", planets)
+  renderPlanets(planets);
 }
 
-const filterCharacters = () => {
+const filterPlanets = () => {
   const searchString = document.querySelector("#searchString").value;
   const re = new RegExp(searchString, "i");
-  matchingCharacters = characters.filter(character => re.test(character.name))
-  renderCharacters(matchingCharacters);
+  matchingPlanets = planets.filter(planet => re.test(planet.name))
+  renderPlanets(matchingPlanets);
 }
 
-const renderCharacters = characters => {
-  const divs = characters.map(character => {
+const renderPlanets = planets => {
+  const divs = planets.map(planet => {
     const el = document.createElement('div');
-    el.addEventListener('click', () => goToCharacterPage(character.id));
-    el.textContent = character.name;
+    el.addEventListener('click', () => goToPlanetPage(planet.id));
+    el.textContent = planet.name;
     return el;
   })
-  charactersList.replaceChildren(...divs)
+  planetsList.replaceChildren(...divs)
 }
 
-const goToCharacterPage = id => window.location = `/character.html?id=${id}`
+const goToPlanetPage = id => window.location = `/planet.html?id=${id}`
--- planet.js
+++ planet.js
@@ -1,64 +1,80 @@
 let nameH1;
-let birthYearSpan;
-let heightSpan;
-let massSpan;
-let filmsDiv;
-let planetDiv;
+let diameterSpan;
+let rotationSpan;
+let orbSpan;
+let populationSpan;
+let climateSpan;
+let terrainSpan;
+let gravitySpan;
+let waterSpan;
 const baseUrl = `http://localhost:9001/api`;
 
 // Runs on page load
 addEventListener('DOMContentLoaded', () => {
   nameH1 = document.querySelector('h1#name');
-  birthYearSpan = document.querySelector('span#birth_year');
-  massSpan = document.querySelector('span#mass');
-  heightSpan = document.querySelector('span#height');
-  homeworldSpan = document.querySelector('span#homeworld');
-  filmsUl = document.querySelector('#films>ul');
+
+  diameterSpan = document.querySelector('span#diameter');
+  rotationSpan = document.querySelector('span#rot_period');
+  orbSpan = document.querySelector('span#orb_period');
+  populationSpan = document.querySelector('span#population');
+ 
+  climateSpan = document.querySelector('span#climate');
+  terrainSpan = document.querySelector('span#terrain');
+  gravitySpan = document.querySelector('span#gravity');
+  waterSpan = document.querySelector('span#has_water');
+  
   const sp = new URLSearchParams(window.location.search)
   const id = sp.get('id')
-  getCharacter(id)
+  getPlanet(id)
 });
 
-async function getCharacter(id) {
-  let character;
+async function getPlanet(id) {
+  let planet;
   try {
-    character = await fetchCharacter(id)
-    character.homeworld = await fetchHomeworld(character)
-    character.films = await fetchFilms(character)
+    planet = await fetchPlanet(id)
+    console.log(planet);
+    // Template for linkable elements
+    // character.homeworld = await fetchHomeworld(character)
+    
+    // character.homeworld = await fetchHomeworld(character)
+    // character.films = await fetchFilms(character)
   }
   catch (ex) {
-    console.error(`Error reading character ${id} data.`, ex.message);
+    console.error(`Error reading planet ${id} data.`, ex.message);
   }
-  renderCharacter(character);
+  renderPlanet(planet[0]);
 
 }
-async function fetchCharacter(id) {
-  let characterUrl = `${baseUrl}/characters/${id}`;
-  return await fetch(characterUrl)
+async function fetchPlanet(id) {
+  let planetUrl = `${baseUrl}/planets?id=${id}`;
+  return await fetch(planetUrl)
     .then(res => res.json())
+
 }
 
-async function fetchHomeworld(character) {
-  const url = `${baseUrl}/planets/${character?.homeworld}`;
-  const planet = await fetch(url)
-    .then(res => res.json())
-  return planet;
-}
+// Template async function for fetching info about
+// async function fetchHomeworld(character) {
+//   const url = `${baseUrl}/planets/${character?.homeworld}`;
+//   const planet = await fetch(url)
+//     .then(res => res.json())
+//   return planet;
+// }
 
-async function fetchFilms(character) {
-  const url = `${baseUrl}/characters/${character?.id}/films`;
-  const films = await fetch(url)
-    .then(res => res.json())
-  return films;
-}
+const renderPlanet = planet => {
+  document.title = `SWAPI - ${planet?.name}`;  // Just to make the browser tab say their name
+  nameH1.textContent = planet?.name;
 
-const renderCharacter = character => {
-  document.title = `SWAPI - ${character?.name}`;  // Just to make the browser tab say their name
-  nameH1.textContent = character?.name;
-  heightSpan.textContent = character?.height;
-  massSpan.textContent = character?.mass;
-  birthYearSpan.textContent = character?.birth_year;
-  homeworldSpan.innerHTML = `<a href="/planet.html?id=${character?.homeworld.id}">${character?.homeworld.name}</a>`;
-  const filmsLis = character?.films?.map(film => `<li><a href="/film.html?id=${film.id}">${film.title}</li>`)
-  filmsUl.innerHTML = filmsLis.join("");
+  diameterSpan.textContent = planet?.diameter;
+  rotationSpan.textContent = planet?.rotation_period;
+  orbSpan.textContent = planet?.orbital_period;
+  populationSpan.textContent = planet?.population;
+
+  climateSpan.textContent = planet?.climate;
+  terrainSpan.textContent = planet?.terrain;
+  gravitySpan.textContent = planet?.gravity;
+
+  waterSpan.textContent = (planet?.surface_water === 1 ? 'Yes' : 'No');
+  
+  // const filmsLis = character?.films?.map(film => `<li><a href="/film.html?id=${film.id}">${film.title}</li>`)
+  // filmsUl.innerHTML = filmsLis.join("");
 }
